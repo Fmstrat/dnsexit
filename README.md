fa8bc44229e869de50f1e3287fe243d40a132dbc
Fmstrat <fmstrat@nospam.nospam>
2018-01-16 18:33:40 -0500

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # dnsexit
 DNSExit dynamic IP client for Docker
+
+```
+version: '2'
+
+services:
+
+  dnsexit:
+    image: dnsexit
+    container_name: dnsexit
+    volumes:
+      - /etc/localtime:/etc/localtime:ro
+    environment:
+      - LOGIN=mylogin
+      - PASSWORD=mypassword
+      - HOST=host.domain.com
+      - INTERVAL=600
+    restart: always
+
+```
---
+++ dnsexit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MYIP="None"
+
+function updateIP() {
+	RESULT=$(curl -s "https://www.dnsexit.com/RemoteUpdate.sv?login=${LOGIN}&password=${PASSWORD}&host=${HOST}")
+	RESULT=$(echo $RESULT)
+	echo $RESULT;
+	if [ "$RESULT" == "HTTP/1.1 200 OK 0=Success" ] || [ "$RESULT" == "HTTP/1.1 200 OK 4=IP not changed. To save our system resources, please don't post updates unless the IP got changed." ]; then
+		MYIP=${IP}
+	fi
+}
+
+function checkIP() {
+	IP=$(curl -s "http://ip.dnsexit.com/")
+	IP=$(echo $IP)
+
+	if [ "$IP" != "$MYIP" ]; then
+		echo "IP has changed from '$MYIP' to '$IP'";
+		updateIP $IP
+	else
+		echo "IP is still '$MYIP'";
+	fi
+	echo "Waiting ${INTERVAL}..."
+	sleep ${INTERVAL}
+}
+
+while true; do
+	checkIP;
+done;
